e63a89dbba7fa5cdd31550cdee646fc7b370384b
Armen Grigorian <info@propertystart.com.au>
2020-03-30 10:15:46 +1100

    Version 1

---
+++ data.js
@@ -0,0 +1,199 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry--living",
+      "name": "Entry & Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8934988927618548,
+        "pitch": -0.018536844724593493,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3341673177388422,
+          "pitch": 0.21167276099275156,
+          "rotation": 4.71238898038469,
+          "target": "1-kitchen"
+        },
+        {
+          "yaw": 0.13679583222128322,
+          "pitch": 0.29975194641891534,
+          "rotation": 6.283185307179586,
+          "target": "2-master-with-en-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6714174775529216,
+        "pitch": 0.045065928670148026,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3409632512489473,
+          "pitch": 0.46547316275835726,
+          "rotation": 4.71238898038469,
+          "target": "0-entry--living"
+        },
+        {
+          "yaw": -1.373627818283662,
+          "pitch": 0.21069635256684904,
+          "rotation": 0,
+          "target": "2-master-with-en-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master-with-en-suite",
+      "name": "Master with En-suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.817776771848263,
+        "pitch": 0.002512054234520278,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom-2",
+      "name": "Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.35152062830899666,
+        "pitch": 0.03998025177187259,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-back-yard",
+      "name": "Back Yard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1967875513675423,
+        "pitch": 0.12017580978707265,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.622037340058645,
+          "pitch": 0.21167949787415452,
+          "rotation": 0.7853981633974483,
+          "target": "1-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20 Peveril St Glen Waverley",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
